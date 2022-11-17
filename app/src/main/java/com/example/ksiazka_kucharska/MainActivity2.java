7537bc23954894669c6cba5e860442a56d8a192b
unknown <--local>
2022-11-17 11:45:29 +0100

    wszystko dziala i dodaje

--- app/src/main/java/com/example/ksiazka_kucharska/MainActivity2.java
+++ app/src/main/java/com/example/ksiazka_kucharska/MainActivity2.java
@@ -1,18 +1,27 @@
 package com.example.ksiazka_kucharska;
 
+import static com.example.ksiazka_kucharska.MainActivity.EXTRA_KATEGORIA;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
+//    public static final String EXTRA_KATEGORIA = "id_kategoria";
 
     ArrayAdapter<Przepis> adapterPrzepisy;
     ListView listViewPrzepisy;
+    public static final String EXTRA_IDPRZEPISU = "id_przepisu";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +34,21 @@ public class MainActivity2 extends AppCompatActivity {
 
         listViewPrzepisy = findViewById(R.id.listView);
         listViewPrzepisy.setAdapter(adapterPrzepisy);
+        listViewPrzepisy.setOnItemClickListener(
+                (adapterView, view, i, l) -> {
+                    Intent intencja = new Intent(MainActivity2.this, MainActivity3.class);
+                    intencja.putExtra(EXTRA_IDPRZEPISU, i);
+                    intencja.putExtra(EXTRA_KATEGORIA,kategoria);
+                    startActivity(intencja);
+                }
+        );
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(view -> {
+            EditText text = findViewById(R.id.editText);
+            String nazwaPrzepisu = text.getText().toString();
+            Przepis przepisDodany = new Przepis(nazwaPrzepisu,2, "safaedthh",R.drawable.lody);
+            wybranePrzepisy.add(przepisDodany);
+            adapterPrzepisy.notifyDataSetChanged();
+        });
     }
 }
--- app/src/main/java/com/example/ksiazka_kucharska/MainActivity3.java
+++ app/src/main/java/com/example/ksiazka_kucharska/MainActivity3.java
@@ -1,14 +1,47 @@
 package com.example.ksiazka_kucharska;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
+    public static final String EXTRA_IDPRZEPISU = "id_przepisu";
+    public static final String EXTRA_KATEGORIA = "id_kategoria";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        int ktoryPrzepis = getIntent().getExtras().getInt(MainActivity2.EXTRA_IDPRZEPISU,0);
+        int kategoria = getIntent().getExtras().getInt(MainActivity.EXTRA_KATEGORIA,0);
+        Przepis przepis = RepozytoriumPrzepisow.wybierzPrzepisy(kategoria).get(ktoryPrzepis);
+        ImageView img = findViewById(R.id.imageView);
+        img.setImageResource(przepis.getIdObrazka());
+        TextView tvTytul  = findViewById(R.id.textViewTytul);
+        TextView tvOpis  = findViewById(R.id.textViewSkladniki);
+        tvTytul.setText(przepis.getNazwa());
+        tvOpis.setText(przepis.getListaSkladnikow());
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(view -> {
+            //Musisz kazda aktywnosc zrobic w manifescie
+            //W manifescie dodac filtr do wysylania sms
+            Intent intencja = new Intent();
+            intencja.setAction(Intent.ACTION_SEND);
+            intencja.putExtra(Intent.EXTRA_TEXT, przepis.getNazwa() + " " + przepis.getListaSkladnikow() + " Smacznego! Jak se zrobisz ");
+            intencja.setType("text/plain");
+            Intent podzielSieIntencja = Intent.createChooser(intencja, null);
+            startActivity(podzielSieIntencja);
+        });
+
+    }
+    protected void onSaveInstanceState(@NonNull Bundle outState){
+        super.onSaveInstanceState(outState);
+
     }
 }
